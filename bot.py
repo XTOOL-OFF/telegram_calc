4e1c52046273e840409bf36d65ece8573ef9f10d
XERSIX <54548160+XTOOL-OFF@users.noreply.github.com>
2020-04-10 01:45:07 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -7,15 +7,16 @@ app_id = "LL29U8-RXX6PHLV5P"
 client = wolframalpha.Client(app_id) 
 bot = telebot.TeleBot('964957577:AAHQlnTDLdyLxDsrnsSE8M0HcxRwMup6YDk')
 
-@bot.message_handler(commands=['Уравнение'])
-def Equation(message):
-    send = bot.send_message(message.chat.id, "Введи уравнение!:")
-    bot.register_next_step_handler(send, user_equation)
-    def user_equation():
-        try:
-            equation = message.text
-            res = client.query(equation)
-            answer = next(res.results).text
-            bot.send_message(message.chat.id, answer)
-        except:
-            print("error")
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+	if message.text.lower() == 'уравнение':
+            send = bot.send_message(message.chat.id, "Введи уравнение!:")
+            bot.register_next_step_handler(send, user_equation)
+                def user_equation():
+                    try:
+                        equation = message.text
+                        res = client.query(equation)
+                        answer = next(res.results).text
+                        bot.send_message(message.chat.id, answer)
+                    except:
+                        print("error")
